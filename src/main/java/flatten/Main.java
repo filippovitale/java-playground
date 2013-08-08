c616ca3a0adaf92a0d5794687831eed9a839eeb2
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-08 20:53:26 +1000

    it works

--- src/main/java/flatten/Main.java
+++ src/main/java/flatten/Main.java
@@ -3,12 +3,12 @@ package flatten;
 public class Main {
 
     public static void main(String[] args) {
-        Tree<Integer> a = new Tree.Leaf<Integer>(1);
+        Tree<Integer> a = new Tree.Leaf<Integer>(10);
         Tree<Integer> b = new Tree.Node<Integer>(
-                new Tree.Leaf<Integer>(5),
-                new Tree.Leaf<Integer>(4),
-                new Tree.Leaf<Integer>(9));
-        Tree<Integer> c = new Tree.Leaf<Integer>(6);
+                new Tree.Leaf<Integer>(50),
+                new Tree.Leaf<Integer>(40),
+                new Tree.Leaf<Integer>(90));
+        Tree<Integer> c = new Tree.Leaf<Integer>(60);
 
         Tree<Integer> tree = new Tree.Node<Integer>(a, b, c);
 
--- src/main/java/flatten/MyFlattenTree.java
+++ src/main/java/flatten/MyFlattenTree.java
@@ -1,11 +1,40 @@
 package flatten;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MyFlattenTree<T> implements FlattenTree<T> {
 
     public List<T> flattenInOrder(Tree<T> tree) {
-        return null;
+        if (tree == null) {
+            throw new IllegalArgumentException();
+        }
+
+        List<T> result = new LinkedList<T>();
+        inOrderTraverse(tree, result);
+        return result;
     }
 
+    private void inOrderTraverse(Tree<T> tree, List<T> result) {
+        Either<T, Triple<Tree<T>>> LeafOrNode = tree.get();
+        boolean isLeaf = LeafOrNode.isLeft();
+
+        Function<T,T> getLeaf = new MyFunction<T, T>();
+
+        if (isLeaf) {
+            result.add(LeafOrNode.ifLeft(getLeaf));
+        } else {
+            inOrderTraverse(LeafOrNode.ifRight(new MyFunction<Triple<Tree<T>>, Triple<Tree<T>>>()).left(), result);
+            inOrderTraverse(LeafOrNode.ifRight(new MyFunction<Triple<Tree<T>>, Triple<Tree<T>>>()).middle(), result);
+            inOrderTraverse(LeafOrNode.ifRight(new MyFunction<Triple<Tree<T>>, Triple<Tree<T>>>()).right(), result);
+        }
+
+    }
+
+}
+
+class MyFunction<P,R> implements Function<P,R> {
+    public R apply(P p) {
+        return (R) p;
+    }
 }
