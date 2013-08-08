8c0aa5c716f4f9e9e201385c61692ce01222b2de
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-08 21:04:02 +1000

    updated tests

--- src/main/java/flatten/Main.java
+++ src/main/java/flatten/Main.java
@@ -3,18 +3,32 @@ package flatten;
 public class Main {
 
     public static void main(String[] args) {
-        Tree<Integer> a = new Tree.Leaf<Integer>(10);
+        Tree<Integer> a = new Tree.Leaf<Integer>(1);
         Tree<Integer> b = new Tree.Node<Integer>(
-                new Tree.Leaf<Integer>(50),
-                new Tree.Leaf<Integer>(40),
-                new Tree.Leaf<Integer>(90));
-        Tree<Integer> c = new Tree.Leaf<Integer>(60);
+                new Tree.Leaf<Integer>(5),
+                new Tree.Leaf<Integer>(4),
+                new Tree.Leaf<Integer>(9));
+        Tree<Integer> c = new Tree.Leaf<Integer>(6);
 
         Tree<Integer> tree = new Tree.Node<Integer>(a, b, c);
 
-        System.out.println(tree);
-        System.out.println("------------------------------------------------");
         System.out.println(new MyFlattenTree<Integer>().flattenInOrder(tree));
+
+        System.out.println("------------------------------------------------");
+
+        tree = new Tree.Node<Integer>(
+                new Tree.Leaf<Integer>(1),
+                new Tree.Node<Integer>(
+                        new Tree.Node<Integer>(
+                                new Tree.Leaf<Integer>(2),
+                                new Tree.Leaf<Integer>(3),
+                                new Tree.Leaf<Integer>(4)),
+                        new Tree.Leaf<Integer>(5),
+                        new Tree.Leaf<Integer>(6)),
+                new Tree.Leaf<Integer>(7));
+
+        System.out.println(new MyFlattenTree<Integer>().flattenInOrder(tree));
+
     }
 
 }
