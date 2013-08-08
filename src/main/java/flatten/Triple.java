57d9f0195afed1a730f95c19b8e390c963eb589d
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-08 20:02:31 +1000

    housekeeping

--- src/main/java/flatten/Triple.java
+++ src/main/java/flatten/Triple.java
@@ -4,25 +4,25 @@ package flatten;
  * A type that stores three values of the same type.
  */
 public class Triple<V> {
-	
-	private final V l, m, r;
 
-	public Triple(V l, V m, V r) {
-		this.l = l;
+    private final V l, m, r;
+
+    public Triple(V l, V m, V r) {
+        this.l = l;
         this.m = m;
-		this.r = r;
-	}
+        this.r = r;
+    }
 
-	public V left() {
-		return l;
-	}
+    public V left() {
+        return l;
+    }
 
     public V middle() {
         return m;
     }
 
-	public V right() {
-		return r;
-	}
-	
+    public V right() {
+        return r;
+    }
+
 }
