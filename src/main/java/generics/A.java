adb7242b77ab8b6ebc79468bd4c0cb05defb33fe
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-17 12:32:35 +1000

    extends and super explained

--- src/main/java/generics/A.java
+++ src/main/java/generics/A.java
@@ -1,19 +1,52 @@
 package generics;
 
-public class A<E> {
+import java.util.ArrayList;
+import java.util.List;
 
-    void doSomething(E e) {
+public class A {
+
+    public static void superCanAdd(List<? super B> lsb) {
+//        lsb.add(new A()); // NO!
+        lsb.add(new B()); // OK
+        lsb.add(new C()); // OK
+//        lsb.add(new Object()); // NO!
+        lsb.add(null); // OK
+
+//        A a = lsb.get(0); // NO!
+//        B b = lsb.get(0); // NO!
+//        C c = lsb.get(0); // NO!
+        Object o = lsb.get(0);
+    }
+
+    public static void extentsCanGet(List<? extends B> leb) {
+//        leb.add(new A()); // NO!
+//        leb.add(new B()); // NO!
+//        leb.add(new C()); // NO!
+//        leb.add(new Object()); // NO!
+        leb.add(null); // OK
+
+        A a = leb.get(0); // OK
+        B b = leb.get(0); // OK
+//        C c = leb.get(0); // NO!
+        Object o = leb.get(0); // OK
     }
 
     public static void main(String[] args) {
+
+        superCanAdd(new ArrayList<A>()); // OK
+        superCanAdd(new ArrayList<B>()); // OK
+//        superCanAdd(new ArrayList<C>()); // NO!
+
+//        extentsCanGet(new ArrayList<A>()); // NO!
+        extentsCanGet(new ArrayList<B>()); // OK
+        extentsCanGet(new ArrayList<C>()); // OK
+
     }
 
 }
 
-class B<D> extends A<D> {
-
+class B extends A {
 }
 
-class C<E> extends B<E> {
-
+class C extends B {
 }
